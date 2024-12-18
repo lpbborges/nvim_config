09e72326435bc957551591883350892644887607
Luiz Paulo <luizpaulobborges@gmail.com>
2024-12-18 13:25:27 -0300

    feat: add blink.cmp

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,5 +1,19 @@
 return {
   "neovim/nvim-lspconfig",
+  dependencies = {
+    'saghen/blink.cmp',
+    {
+      "folke/lazydev.nvim",
+      ft = "lua", -- only load on lua files
+      opts = {
+        library = {
+          -- See the configuration section for more details
+          -- Load luvit types when the `vim.uv` word is found
+          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        },
+      },
+    },
+  },
   keys = {
     { "<leader>vd",  function() vim.diagnostic.open_float() end },
     { "[d",          function() vim.diagnostic.goto_prev() end },
@@ -14,9 +28,10 @@ return {
     { "<leader>vrn", function() vim.lsp.buf.rename() end },
   },
   config = function()
+    local capabilities = require('blink.cmp').get_lsp_capabilities()
     local lspconfig = require "lspconfig"
-    lspconfig.lua_ls.setup {}
-    lspconfig.ts_ls.setup {}
+    lspconfig.lua_ls.setup { capabilities = capabilities }
+    lspconfig.ts_ls.setup { capabilities = capabilities }
     vim.diagnostic.config({
       signs = {
         text = {
