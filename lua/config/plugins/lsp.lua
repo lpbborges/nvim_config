7c1d4dad237ca412654e264c42f1e9b23c20e16a
Luiz Paulo <luizpaulobborges@gmail.com>
2025-06-08 18:19:58 -0300

    remove lua_ls setup call

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -69,7 +69,6 @@ return {
         setup_keymaps()
         local capabilities = require("blink.cmp").get_lsp_capabilities()
         local lspconfig = require "lspconfig"
-        lspconfig.lua_ls.setup { capabilities = capabilities }
         lspconfig.ts_ls.setup { capabilities = capabilities }
         lspconfig.biome.setup {
             -- Optional custom configuration
@@ -105,7 +104,9 @@ return {
         vim.api.nvim_create_autocmd("LspAttach", {
             callback = function(args)
                 local client = vim.lsp.get_client_by_id(args.data.client_id)
-                if not client then return end
+                if not client then
+                    return
+                end
                 if client.supports_method "textDocument/formatting" then
                     vim.api.nvim_create_autocmd("BufWritePre", {
                         buffer = args.buf,
