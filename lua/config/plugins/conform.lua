c8c412e646722adbe7d49c94d7fe0bc23a5b48cb
LP <luizpaulobborges@gmail.com>
2025-07-01 21:40:06 -0300

    added code format for elixir files

--- lua/config/plugins/conform.lua
+++ lua/config/plugins/conform.lua
@@ -1,26 +1,29 @@
 return {
-  "stevearc/conform.nvim",
-  config = function()
-    require("conform").setup({
-      formatters = {
-        -- Custom formatter for trimming trailing whitespace
-        trim_whitespace = {
-          command = "sed",
-          args = { "-E", "s/[[:space:]]+$//g" },
-          stdin = true,
-        },
-      },
-      formatters_by_ft = {
-        -- Biome for JavaScript ecosystem
-        javascript = { "biome" },
-        typescript = { "biome" },
-        javascriptreact = { "biome" },
-        typescriptreact = { "biome" },
-        json = { "biome" },
-        lua = { "stylua" },
-        -- Add trim_whitespace for other file types
-        ["*"] = { "trim_whitespace" }, -- Apply to all other filetypes
-      },
-    })
-  end,
+    "stevearc/conform.nvim",
+    config = function()
+        require("conform").setup {
+            formatters = {
+                -- Custom formatter for trimming trailing whitespace
+                trim_whitespace = {
+                    command = "sed",
+                    args = { "-E", "s/[[:space:]]+$//g" },
+                    stdin = true,
+                },
+            },
+            formatters_by_ft = {
+                -- Biome for JavaScript ecosystem
+                javascript = { "biome" },
+                typescript = { "biome" },
+                javascriptreact = { "biome" },
+                typescriptreact = { "biome" },
+                json = { "biome" },
+                lua = { "stylua" },
+                elixir = { "mix" },
+                heex = { "mix" },
+                eelixir = { "mix" }, -- For embedded Elixir in templates
+                -- Add trim_whitespace for other file types
+                ["*"] = { "trim_whitespace" }, -- Apply to all other filetypes
+            },
+        }
+    end,
 }
