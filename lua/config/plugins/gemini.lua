bcb2bcacbdbae5ddbdab7824fcad1a5ceae53f29
LP <luizpaulobborges@gmail.com>
2025-11-14 20:47:44 -0300

    add gemini

---
+++ lua/config/plugins/gemini.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    'kiddos/gemini.nvim',
+    opts = {
+      completion = {
+        enabled = true,
+      },
+      chat = {
+        enabled = true,
+      },
+      hints = {
+        enabled = true,
+      },
+    },
+  },
+}
