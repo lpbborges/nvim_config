0cfa38c7e0d9a13d2420c5eb1dd96c2a904a6109
LP <luizpaulobborges@gmail.com>
2026-01-09 20:04:36 -0300

    gemini config

--- lua/config/plugins/gemini.lua
+++ lua/config/plugins/gemini.lua
@@ -4,12 +4,14 @@ return {
     opts = {
       completion = {
         enabled = true,
+        completion_delay = 500,
       },
       chat = {
         enabled = true,
       },
       hints = {
         enabled = true,
+        hints_delay = 1000,
       },
     },
   },
