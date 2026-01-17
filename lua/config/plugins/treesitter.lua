deb86aa50524fdcdf1f430c7ab45c06c92e906b8
LP <luizpaulobborges@gmail.com>
2026-01-17 13:28:02 -0300

    fix(treesitter): update configs for compatibility

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         config = function()
-            require("nvim-treesitter.configs").setup {
+            require("nvim-treesitter").setup {
                 sync_install = true,
                 ensure_installed = {
                     "bash",
