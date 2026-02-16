8a03b988c012b660a3c5e615840ef5f651ea587a
LP <luizpaulobborges@gmail.com>
2026-02-16 15:45:04 -0300

    chore: increase max_filesize

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -24,8 +24,8 @@ return {
                 auto_install = true,
                 highlight = {
                     enable = true,
-                    disable = function(lang, buf)
-                        local max_filesize = 100 * 1024 -- 100 KB
+                    disable = function(_, buf)
+                        local max_filesize = 100 * 2024 -- 200 KB
                         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
                         if ok and stats and stats.size > max_filesize then
                             return true
