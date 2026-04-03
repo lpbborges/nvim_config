3ab2b500a6804e358f35354b6701d027030d8b34
LP <luizpaulobborges@gmail.com>
2026-04-03 17:50:35 -0300

    fix(treesitter): pin to master branch for Neovim 0.11 compatibility

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        branch = "master",
         build = ":TSUpdate",
         config = function()
             require("nvim-treesitter.configs").setup {
@@ -24,7 +25,7 @@ return {
                 auto_install = true,
                 highlight = {
                     enable = true,
-                    disable = function(lang, buf)
+                    disable = function(_lang, buf)
                         local max_filesize = 100 * 1024 -- 100 KB
                         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
                         if ok and stats and stats.size > max_filesize then
