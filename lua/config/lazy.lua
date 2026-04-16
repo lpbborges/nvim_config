ab2d873718849ee15cbcb539f5b42bb5ff6fbd1b
LP <luizpaulobborges@gmail.com>
2026-04-16 18:00:14 -0300

    feat(dev): load neon-genesis from local path for development

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,6 +19,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   change_detection = { notify = false },
+  dev = { path = "~/code/auto" },
   spec = {
     { import = "config.plugins" },
   },
