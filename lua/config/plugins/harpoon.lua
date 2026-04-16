f2af9db95c8df6b892162a99227226f7fcbbc5da
LP <luizpaulobborges@gmail.com>
2026-04-16 17:40:28 -0300

    feat(harpoon): add numbered navigation keymaps <leader>1-4

--- lua/config/plugins/harpoon.lua
+++ lua/config/plugins/harpoon.lua
@@ -8,5 +8,10 @@ return {
 
         vim.keymap.set("n", "<S-m>", function() harpoon:list():add() end, { desc = "Harpoon: Add file" })
         vim.keymap.set("n", "<TAB>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon: Menu" })
+
+        vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Harpoon: File 1" })
+        vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Harpoon: File 2" })
+        vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "Harpoon: File 3" })
+        vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "Harpoon: File 4" })
     end,
 }
